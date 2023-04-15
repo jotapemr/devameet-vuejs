ec9d7257992463d50f5470eede692c9a8292b941
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-04-15 11:14:13 -0300

    Login final

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,6 +3,10 @@
     import {defineComponent} from 'vue';
     import loginIcon from '../assets/images/mail.svg';
     import passwordIcon from '../assets/images/key.svg';
+    import { LoginServices } from '@/services/LoginServices';
+    import router from '@/router';
+
+    const loginServices = new LoginServices();
 
     export default defineComponent({
     components: { CustomInput },
@@ -27,6 +31,39 @@
         buttonText(){
             return this.loading ? 'Carregando...' : 'Login'
         }
+    },
+
+    methods: {
+        setLogin(v:string){
+            this.login = v
+        },
+        setPassword(v:string){
+            this.password = v
+        },
+        async doLogin(){
+            try{
+                console.log(this.login)
+                console.log(this.password)
+                if(!this.login || !this.login.trim() || !this.password || !this.password.trim()){
+                    return this.error = 'Favor informar usuário e senha.';
+                }
+
+                this.loading = true;
+                await loginServices.login({login: this.login, password: this.password});
+                this.loading = false;
+                return router.push('/');
+            }catch(e: any){
+                console.log('erro ao efeutar login:', e)
+
+                if(e?.response?.data?.message){
+                    this.error = e?.response?.data?.message;
+                }else{
+                    this.error = 'Não foi possível efeutar login, tente novamente!'
+                }
+            }
+
+            this.loading = false;
+        }
     }
 })
 </script>
@@ -36,10 +73,10 @@
         <img src="../assets/images/logo.svg" alt="logo devameet" class="logo"/>
         <form>
             <p v-if="error" class="error">{{ error }}</p>
-            <CustomInput :icon="loginIcon" alt="login" name="login" placeholder="Login" type="text" :model-value="login"/>
-            <CustomInput :icon="passwordIcon" alt="senha" name="senha" placeholder="Senha" type="password" :model-value="password"/>
+            <CustomInput :icon="loginIcon" alt="login" name="login" placeholder="Login" type="text" :model-value="login" @setInput="setLogin"/>
+            <CustomInput :icon="passwordIcon" alt="senha" name="senha" placeholder="Senha" type="password" :model-value="password" @setInput="setPassword"/>
             
-            <button>{{ buttonText }}</button>
+            <button type="button" @click="doLogin">{{ buttonText }}</button>
             <div class="link">
                 <p>Não Possui uma conta?</p>
                 <RouterLink to="/register">Faça seu cadastro agora!</RouterLink>
