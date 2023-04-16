97f3af50437bd64c55e1c1158f82d6f7caf4071f
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-04-16 19:44:52 -0300

    principal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
 import HeaderVue from '@/components/general/Header.vue';
 import FooterVue from '@/components/general/Footer.vue';
+import MeetHomeVue from '@/components/meet/MeetHome.vue'
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    components: { HeaderVue, FooterVue}
+    components: { HeaderVue, FooterVue, MeetHomeVue}
 });
 </script>
 
 <template>
     <HeaderVue/>
+    <MeetHomeVue/>
     <FooterVue/>
 </template>
 
