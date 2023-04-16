32a044899eef7e42489ace34c32c4f616e697d8f
jotapemr <101481431+jotapemr@users.noreply.github.com>
2023-04-16 14:41:37 -0300

    conclusion public pages

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import {GDialog} from 'gitart-vue-dialog';
 
-import './assets/styles/global.scss'
+import 'gitart-vue-dialog/dist/style.css';
+import './assets/styles/global.scss';
 
 const app = createApp(App)
 
+app.component('GDialog', GDialog);
 app.use(createPinia())
 app.use(router)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginViewVue from '@/views/LoginView.vue'
+import RegisterViewVue from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginViewVue,
-    }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterViewVue,
+    },
   ]
 })
 
